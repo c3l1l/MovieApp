1f48148477bbc4df56b665d25401444779c2906e
Celil Ozturk <celil.ozturk@windowslive.com>
2023-04-13 00:54:02 +0200

    CreateUserDtoValidator

---
+++ Movie.Service/Validations/CreateUserDtoValidator.cs
@@ -0,0 +1,20 @@
+﻿using FluentValidation;
+using MovieApp.Core.DTOs;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MovieApp.Service.Validations
+{
+    public class CreateUserDtoValidator:AbstractValidator<CreateUserDto>
+    {
+        public CreateUserDtoValidator()
+        {
+            RuleFor(u => u.Email).NotEmpty().WithMessage("Email is required").EmailAddress().WithMessage("Email is wrong");
+            RuleFor(u => u.Password).NotEmpty().NotEmpty().WithMessage("Password is required");
+            RuleFor(u => u.UserName).NotEmpty().WithMessage("UserName is required");
+        }
+    }
+}
